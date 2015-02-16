a45fe5b2b3266b3050d48792e3fad7aa284f9804
Kushal Khandelwal <kushal124@gmail.com>
2015-02-16 15:57:30 +0100

    Test:Different Models

--- code/drive_tests.py
+++ code/drive_tests.py
@@ -5,7 +5,7 @@ from skimage.filter import threshold_otsu as totsu
 import numpy as np
 from skimage.color import rgb2hsv, rgb2lab, rgb2luv
 
-def testrun(patchsize=(10,10),clusters=100):
+def drive_model(patchsize=(10,10),clusters=100):
 
     # Training Patches
     img = driveUtils.readimage('../training/images/')
@@ -34,6 +34,8 @@ def testrun(patchsize=(10,10),clusters=100):
 
     greenIdx = kmG.predict(greenPatch)
 
+    greenCluster = {}
+
     for i in range(clusters):
         greenCluster[i] = []
 
@@ -48,3 +50,31 @@ def testrun(patchsize=(10,10),clusters=100):
 
 
     return kmG,clusterGtG
+
+def test_predict(kmG,clusterGtG,location,patchsize=(10,10)):
+
+    test_img = driveUtils.readimage('../test/images/')
+    testPatchG = driveUtils.computePatch(test_img, channel=1,size=patchsize)
+
+    if not os.path.exists('../Results/'+str(location)):
+        os.makedirs('../Results/'+str(location))    
+
+    for key in test_img.keys():     
+        tPatchG = driveUtils.zscore_norm(driveUtils.flattenlist(testPatchG[key]))
+        # print "Debug Level 1.2"
+        patchGidx = kmG.predict(tPatchG)
+        # print "Debug Level 3"
+        testimg = patchify.unpatchify(
+            np.asarray([clusterGtG[j] for i, j in enumerate(patchGidx)]), (584, 565))
+
+
+        print "Saving_" + str(key)
+
+        plt.imsave('../Results/'+str(location)+'/' + str(key) + '_G' +
+                   '.png', testimg, cmap=cm.gray)
+
+####################################
+
+# Model 1
+
+km,clusterModel = drive_model(patchsize=(10,10),clusters=100)
