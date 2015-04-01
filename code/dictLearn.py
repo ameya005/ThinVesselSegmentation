b5b2b78b4967e1c8143ee9093d4292ad80737671
Kushal Khandelwal <kushal124@gmail.com>
2015-04-01 18:00:30 +0200

    Dict Update

--- code/dictLearn.py
+++ code/dictLearn.py
@@ -16,16 +16,14 @@ import cv2
 import skimage.transform as skt
 from sklearn.decomposition import MiniBatchDictionaryLearning,DictionaryLearning
 import random
+import driveUtils
 
-def dictlearn():
+def dictlearn(patchsize=(10,10),clusters=100,clahe=False,rescale=1,thres = 0):
 
 	## Extracting patches
     img = driveUtils.readimage('../training/images/')
     img = driveUtils.dictimgscale(img,scaling=rescale) #Scaling images
 
-    if clahe:
-        img = driveUtils.eq_clahe(img,tilesize=(24,24))
-     
     # Segmentation Patches
     imgGT = driveUtils.readimage('../training/1st_manual/')
     imgGT = driveUtils.dictimgscale(imgGT,scaling=rescale) # Scaling
@@ -54,23 +52,40 @@ def dictlearn():
     ##################################################################
     # Learn Dictionary
 
-    km = MiniBatchDictionaryLearning(n_jobs=-1,n_components=500,fit_algorithm='cd',transform_algorithm='lasso_cd',verbose=2,random_state=42)
+    #km = MiniBatchDictionaryLearning(n_jobs=-1,n_components=500,fit_algorithm='cd',transform_algorithm='lasso_cd',verbose=2,random_state=42)
+    km = MiniBatchDictionaryLearning(n_jobs=-1,n_components=clusters,fit_algorithm='cd',transform_algorithm='threshold',verbose=2,random_state=42,transform_alpha=0.2)
+
     km.fit(greenPatch)
 
     #################################################################
     greenIdx = km.transform(greenPatch)
+
+    # Work around
+    
+    idx = (greenIdx > thres).astype(int)
+    
+    # sum of the idx ( gives number of elements used to construct the dictGt)
+    idxSum = idx.T.sum(axis=1)
+
+
     groundPatch = np.array(greenPatchGT)
 
     dictComponents = km.components_
-    dictGround = np.dot(greenIdx.T,groundPatch)
+    
+    # Dict Ground is obtained here
 
-def test_predict(kmG,clusterGtG,location,patchsize=(10,10),rescale=1,clahe=False):
+    dictGround = np.dot(greenIdx.T,groundPatch)
+    # Normalize the dictGround 
+    groundDict = dictGround / idxSum
+
+    return km,groundDict
+
+def test_predict(km,groundDict,location,patchsize=(10,10),rescale=1,clahe=False,thres=0):
 
     test_img = driveUtils.readimage('../test/images/')
     test_img = driveUtils.dictimgscale(test_img,scaling=1) #Scaling
     
-    if clahe:
-        test_img = driveUtils.eq_clahe(test_img,tilesize=(24,24))
+
     
     #Determine Size of image
     a,b,c = test_img['11'].shape
@@ -78,21 +93,27 @@ def test_predict(kmG,clusterGtG,location,patchsize=(10,10),rescale=1,clahe=False
     testPatchG = driveUtils.computePatch(test_img, channel=1,size=patchsize)
 
     if not os.path.exists('../Results/'+str(location)):
-        os.makedirs('../Results/'+str(location))    
+        os.makedirs('../Results/'+str(location))
 
     for key in test_img.keys():     
         tPatchG = driveUtils.zscore_norm(driveUtils.flattenlist(testPatchG[key]))
         # tPatchG = driveUtils.flattenlist(testPatchG[key])
         # print "Debug Level 1.2"
-        patchGidx = (cl1.predict(tPatchG)).astype('uint8')
+        patchGidx = (km.transform(tPatchG))
+        patchGidx = (patchGidx >thres).astype(int)
+        
         # print "Debug Level 3"
-        testimg = patchify.unpatchify(
-            np.asarray([clusterGtG[j] for i, j in enumerate(patchGidx)]), (a,b))
+        testimg = np.dot(patchGidx,groundDict)
+        testimg = testimg / (patchGidx.sum(axis=1))
+
+        img = patchify.unpatchify(testimg,(a,b))
 
         #testimg = resize(testimg, (584,565))
 
         print "Saving_" + str(key)
 
         plt.imsave('../Results/'+str(location)+'/' + str(key) + '_G' +
-                   '.png', testimg, cmap=cm.gray)
+                   '.png', img, cmap=cm.gray)
 
+with open('dictModel', 'wb') as fp:
+    pickle.dump([km,dictGround], fp)
