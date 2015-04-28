48d27a358283744ffe744d1b5bd1719dfd6f02c3
Kushal Khandelwal <kushal124@gmail.com>
2015-04-28 21:26:41 +0200

    Provide utility for random sampling

--- retinopathy/utils.py
+++ retinopathy/utils.py
@@ -1,5 +1,11 @@
+import random
+
 __author__ = 'kushal'
 """
 Provides utilies and preporcessing facilities
 """
 
+
+def compute_random(X, y, samples=10000, seed=42):
+    for key in X.keys():
+        rnumber = random.sample(xrange(len(X[key])), samples)
