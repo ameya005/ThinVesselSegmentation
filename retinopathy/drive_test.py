fb5bd396c012136c9388cf18f21b916d39a45850
Kushal Khandelwal <kushal124@gmail.com>
2015-04-28 21:01:37 +0200

    Fix

--- retinopathy/drive_test.py
+++ retinopathy/drive_test.py
@@ -1,16 +1,18 @@
+#!/usr/bin/env python
 __author__ = 'kushal'
 
 from datasets import Drive
 from models import KmeansClusterLearn
 
-path_train = '../training'
-path_test = '../test'
-patch_size = (10, 10)
-channel = 1
-ravel = 1
-clusters = 500
-img_size = (584, 565)
-Drive_train = Drive(path_train)
-Drive_train.compute_patch(size=patch_size, channel=channel, ravel=ravel)
+if __name__ == __main__:
+    path_train = '../training'
+    path_test = '../test'
+    patch_size = (10, 10)
+    channel = 1
+    ravel = 1
+    clusters = 500
+    img_size = (584, 565)
+    Drive_train = Drive(path_train)
+    Drive_train.compute_patch(size=patch_size, channel=channel, ravel=ravel)
 
-kmmodel = KmeansClusterLearn(n_clusters=clusters, patch_size=patch_size, image_size=img_size)
+    kmmodel = KmeansClusterLearn(n_clusters=clusters, patch_size=patch_size, image_size=img_size)
