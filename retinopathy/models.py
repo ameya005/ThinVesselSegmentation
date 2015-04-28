0889cafc1c44eb55eb14d3c0acbb772d6c850841
Kushal Khandelwal <kushal124@gmail.com>
2015-04-28 16:47:51 +0200

    AbstractMethods fits and predict in BaseModel

--- retinopathy/models.py
+++ retinopathy/models.py
@@ -1,3 +1,5 @@
+import abc
+
 __author__ = 'kushal'
 """
 Provides different learning models
@@ -8,11 +10,24 @@ class BaseModel(object):
     """
     Base Settings for the learning model
     """
-    pass
+    __metaclass__ = abc.ABCMeta
 
     def __init__(self, training, test):
         self.training = training
         self.test = test
+
+    @abc.abstractmethod
+    def fit(self):
+        """
+        All models must define a fit method.
+        This method would train the algorithm.
+
+        """
+        pass
+
+    @abc.abstractmethod
+    def predict(self):
+        pass
 
 
 class DictLearn(BaseModel):
@@ -40,4 +55,15 @@ class ClusterLearn(BaseModel):
     """
     Provides for the Cluster Learning Method
     """
-    pass
+
+    def __init__(self, training, test, n_clusters, batch_size, reassignment_rato=None):
+        super(ClusterLearn, self).__init__(training, test)
+        self.n_clusters = n_clusters
+        self.batch_size = batch_size
+        self.reassignemnt_ratio = reassignment_rato
+
+    def fit(self):
+        pass
+
+    def predict(self):
+        pass
