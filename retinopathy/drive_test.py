aab2136047970845f80d6476546c63576340ea17
Kushal Khandelwal <kushal124@gmail.com>
2015-04-29 22:28:37 +0200

    Testing Procedure updated

--- retinopathy/drive_test.py
+++ retinopathy/drive_test.py
@@ -3,6 +3,7 @@ __author__ = 'kushal'
 
 from datasets import Drive
 from models import KmeansClusterLearn
+import utils
 
 if __name__ == "__main__":
     path_train = '../training'
@@ -18,4 +19,7 @@ if __name__ == "__main__":
     Drive_train.compute_patch(size=patch_size, channel=channel, ravel=ravel)
     Drive_train.compute_gt_mask(size=patch_size, mask=1, ravel=1)
 
+    patch_train, patch_gt_train = utils.compute_random(Drive_train.patches, Drive_train.patchesGT)
+
     kmmodel = KmeansClusterLearn(n_clusters=clusters, patch_size=patch_size, image_size=img_size)
+    kmmodel.fit(patch_train, patch_gt_train)
