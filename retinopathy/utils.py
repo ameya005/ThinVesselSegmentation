c580be771f42c27b6ad4e399ab7b469e5b340229
Kushal Khandelwal <kushal124@gmail.com>
2015-04-29 22:28:21 +0200

    Provide utility for random sampling

--- retinopathy/utils.py
+++ retinopathy/utils.py
@@ -1,4 +1,5 @@
 import random
+import numpy as np
 
 __author__ = 'kushal'
 """
@@ -6,6 +7,16 @@ Provides utilies and preporcessing facilities
 """
 
 
+# noinspection PyArgumentList
 def compute_random(X, y, samples=10000, seed=42):
+    x_new = list
+    y_new = list
     for key in X.keys():
         rnumber = random.sample(xrange(len(X[key])), samples)
+        x_new.extend(X[key][rnumber])
+        y_new.extend(y[key][rnumber])
+    x_new = np.asarray(x_new)
+    y_new = np.asarray(y_new)
+
+    return x_new, y_new
+
